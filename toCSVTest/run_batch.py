777a0f973f4c8d225f4caf74b9e7edfb054bd4d6
Forward <Julia.Forward@k202domain.com>
2025-03-21 13:40:48 -0230

    Cleaned up run_batch

--- toCSVTest/run_batch.py
+++ toCSVTest/run_batch.py
@@ -1,19 +1,9 @@
 import os
-# import shutil
-# import glob
 import subprocess
 
-subprocess.Popen(['FitSDKRelease_21.158.00\\java\\FitToCSV.bat', "..\\output\\fit\\10738014183.fit"])
-
-
+# Specify directory containing files to be converted
 directory_in_str = "..\\output\\fit\\"
 directory_in = os.fsencode(directory_in_str)
-
-directory_out_str = "..\\output\\csv\\"
-
-# Make an output directory if it does not exist
-if not os.path.isdir(directory_out_str):
-    os.makedirs(directory_out_str)
 
 # run the batch file on each file in the input directory that ends with .fit
 for file in os.listdir(directory_in):
@@ -24,7 +14,6 @@ for file in os.listdir(directory_in):
             p.stdin.write('\n')
             p.stdin.flush()
             p.wait()
-            print(filename)
 
 
 

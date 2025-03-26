fa8dae0a404fa7dce000051a22ce2e02c11d523b
justin.klinkow <jklinkow@yahoo.com>
2025-03-26 09:59:55 -0230

    Updated csv_condenser.py to create a file with fewer columns

--- toCSVTest/csv_condenser.py
+++ toCSVTest/csv_condenser.py
@@ -10,19 +10,22 @@ if not os.path.exists(directory_out_str):
 
 csv_files = glob.glob(directory_in_str+'\\*_data.{}'.format('csv'))
 out = pd.DataFrame()
-output_file = directory_out_str + 'output_file.csv'
+output_file = directory_out_str + 'output_test2.csv'
 first_iteration = True
 count = 0
 for file in csv_files:
     count += 1
     df = pd.read_csv(file, low_memory=False)
     file_name = file.split('\\')[-1]
-    array = ['Workout_'+str(count)] * df.shape[0]
+    # array = ['Workout_'+str(count)] * df.shape[0]
+    array = [file_name] * df.shape[0]
     df['ID'] = array
     if first_iteration:
         out = df
         first_iteration = False
     else:
         out = pd.concat([out, df], axis=0, join='outer')
+out=out[['record.timestamp[s]', 'record.distance[m]', 'record.heart_rate[bpm]', 'record.power[watts]', 'record.enhanced_speed[m/s]', 'ID']]
+out.rename(columns={'record.timestamp[s]': 'timestamp[s]', 'record.distance[m]': 'distance[m]', 'record.heart_rate[bpm]': 'heart_rate[bpm]', 'record.power[watts]': 'power[watts]', 'record.enhanced_speed[m/s]': 'speed[m/s]'}, errors="raise", inplace=True)
 out.to_csv(output_file, index=False)
 print("Complete")
